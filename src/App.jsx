43b8d4ac713f440ca7a82ec2bd654156cdf11b2f
Mahmoud Elzeny <mahmoudelziny20@gmail.com>
2025-02-02 18:13:10 +0200

    implementing starter components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import HomePage from './pages/HomePage';
+import PageNotFound from './pages/PageNotFound';
+import AppLayout from './pages/AppLayout';
+import Login from './pages/Login';
 
 export default function App() {
   return (
-    <div>WorldWise</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
